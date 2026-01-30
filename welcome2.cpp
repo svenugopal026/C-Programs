1ea80d0ff3fc3205f87c73f9d4bbdf560c2c8239
svenugopal026 <75471876+svenugopal026@users.noreply.github.com>
2026-01-30 10:05:08 +0530

    Create welcome2.cpp

---
+++ welcome2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	cout<<"Welcome to C++";
+		cout<<"Github Created ";
+	return 0;
+
+}
+
+// Output:
+
+// Welcome to C++
+//Github Created
